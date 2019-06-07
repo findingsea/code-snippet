471e1bf78224ef59b4e4107852fcd5b2b0402994
finingsea <findingsea@163.com>
2019-06-07 18:08:35 +0800

    more comment

--- polymorphism/polymorphism.cc
+++ polymorphism/polymorphism.cc
@@ -9,6 +9,12 @@ public:
   void Run() {
     cout << "Car is running..." << endl;
   }
+  virtual ~Car() {
+    cout << "Set price to zero..." << endl;
+    price = 0;
+  }
+private:
+  int price = 1000;
 };
 
 class Benz : public Car {
@@ -20,6 +26,12 @@ public:
   void Run() {
     cout << "Benz is running..." << endl;
   }
+  ~Benz() {
+    cout << "Set benz discount to zero..." << endl;
+    discount = 0;
+  }
+private:
+  int discount = 1;
 };
 
 class Tree {
@@ -27,12 +39,18 @@ public:
   virtual void Grownup() {
     cout << "Growning up..." << endl;
   }
+  ~Tree() {
+    cout << "Discontruct Tree..." << endl;
+  }
 };
 
 class Sequoia : public Tree {
 public:
   void Grownup() {
     cout << "Sequoia is growning up..." << endl;
+  }
+  ~Sequoia() {
+    cout << "Discount Sequoia..." << endl;
   }
 };
 
@@ -44,4 +62,6 @@ int main() {
   benz->Run();
   Tree *sequois = new Sequoia();
   sequois->Grownup();
+  delete(benz);  // 会调用子类Benz的析构，再调用基类Car的析构
+  delete(sequois);  // 只会调用基类Tree的析构，潜在风险造成子类Sequoia的泄露
 }
