13a1b4548ff1602e3b96f647c0c9cf26aecf653e
finingsea <findingsea@163.com>
2019-06-07 17:16:58 +0800

    new sinppet

--- polymorphism/polymorphism.cc
+++ polymorphism/polymorphism.cc
@@ -6,6 +6,9 @@ using namespace std;
 class Car {
 public:
   virtual int Start() = 0;
+  void Run() {
+    cout << "Car is running..." << endl;
+  }
 };
 
 class Benz : public Car {
@@ -13,6 +16,9 @@ public:
   int Start() {
     cout << "Benz starts sucessfully." << endl;
     return 0;
+  }
+  void Run() {
+    cout << "Benz is running..." << endl;
   }
 };
 
@@ -30,10 +36,12 @@ public:
   }
 };
 
+// 当使用类的指针调用成员函数时，普通函数由指针类型决定，而虚函数由指针指向的实际类型决定
 int main() {
   // Benz *benz = new Car();  // 编译失败，car是抽象类，无法实例化
   Car *benz = new Benz();
   benz->Start();
+  benz->Run();
   Tree *sequois = new Sequoia();
   sequois->Grownup();
 }
