f2affb5f1c8d803a65dc0bc304579eb49444f2a2
finingsea <findingsea@163.com>
2019-07-01 20:42:52 +0800

    new snippet

---
+++ thread_local/thread_local.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <random>
+#include <thread>
+#include <sstream>
+using namespace std;
+void GetCount() {
+  // count贯穿thread的声明周期
+  thread_local int count = 0;
+  ostringstream ostr;
+  ostr << "Thread id: " << this_thread::get_id() << " " << count++ << endl;
+  cout << ostr.str();
+}
+void GenCount() {
+  for (int i = 0; i < 5; ++i) {
+    GetCount();
+  }
+}
+int main() {
+  thread threads[5];
+  for (int i = 0; i < 5; ++i) {
+    threads[i] = thread(GenCount);
+  }
+  for (auto &t : threads) {
+    t.join();
+  }
+}
