4b7efc95cfe784ebc387916fccc00a719ac3555c
finingsea <findingsea@163.com>
2019-08-27 11:51:19 +0800

    new snippet

---
+++ Trie/trie.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <memory>
+using namespace std;
+
+#define ALPHABET_SIZE 26
+
+struct TrieNode {
+  bool is_key = false;
+  shared_ptr<TrieNode> children[ALPHABET_SIZE];
+};
+
+class TrieTree {
+public:
+  TrieTree() {
+    root = make_shared<TrieNode>();
+  }
+  void Insert(string key) {
+    auto node = root;
+    for (int i = 0; i < key.length(); ++i) {
+      if (!node->children[key[i] - 'a']) {
+        node->children[key[i] - 'a'] = make_shared<TrieNode>();
+      }
+      node = node->children[key[i] - 'a'];
+    }
+    node->is_key = true;
+  }
+  bool Search(string key) {
+    auto node = root;
+    int idx = 0;
+    for (; idx < key.length(); ++idx) {
+      if (!node->children[key[idx] - 'a']) {
+        return false;
+      }
+      node = node->children[key[idx] - 'a'];
+    }
+    return idx == key.length() && node->is_key;
+  }
+  void Print() {
+    queue<shared_ptr<TrieNode> > qu;
+    qu.push(root);
+    while (!qu.empty()) {
+      int count = qu.size();
+      while (count > 0) {
+        auto node = qu.front();
+        qu.pop();
+        --count;
+        for (int i = 0; i < ALPHABET_SIZE; ++i) {
+          if (node->children[i] != nullptr) {
+            cout << (char) (i + 'a') << " ";
+            qu.push(node->children[i]);
+          }
+        }
+        cout << ";  ";
+      }
+      cout << endl;
+    }
+  }
+private:
+  shared_ptr<TrieNode> root;
+};
+
+int main() {
+  TrieTree trie_tree;
+  trie_tree.Insert("find");
+  trie_tree.Insert("found");
+  trie_tree.Print();
+  cout << "Search 'find': " << trie_tree.Search("find") << endl;
+  cout << "Search 'finding': " << trie_tree.Search("finding") << endl;
+}
